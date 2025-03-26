9849ca2c3766c0a546dcdc5b0cfe4315419b6dcf
Altay Hodoglugil <altay.hodo@gmail.com>
2025-03-26 13:30:08 -0600

    Add herbie profile

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,12 +1,12 @@
-import Profile from "../components/Profile";
-import styles from "../styles/Team.module.css";
+import Profile from '../components/Profile';
+import styles from '../styles/Team.module.css';
 
 export default function Team() {
   return (
     <div className={styles.teamContainer}>
       <h1 className={styles.title}>Meet Our Team</h1>
       <div className={styles.teamGrid}>
-      <Profile
+        <Profile
           name="Jeysson "
           description="Top 5 people ever."
           image1="/jason-1.webp"
@@ -23,6 +23,12 @@ export default function Team() {
           description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde cumque aliquam, reprehenderit eaque id rem fugiat ea, adipisci libero maiores dolorum sit amet deleniti non doloremque rerum? Officiis, accusamus atque."
           image1="/altay-1.webp"
           image2="/altay-2.webp"
+        />
+        <Profile
+          name="Herbert Matthew Belloso Hernandez"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde cumque aliquam, reprehenderit eaque id rem fugiat ea, adipisci libero maiores dolorum sit amet deleniti non doloremque rerum? Officiis, accusamus atque."
+          image1="/herbie-1.webp"
+          image2="/herbie-2.webp"
         />
       </div>
     </div>
