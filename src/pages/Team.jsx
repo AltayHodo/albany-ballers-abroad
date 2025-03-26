d552a214848ef4f30277ceb106b22d28ca008543
Altay Hodoglugil <altay.hodo@gmail.com>
2025-03-26 13:19:19 -0600

    add ui changes on mobile, and add jason profile

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -6,6 +6,12 @@ export default function Team() {
     <div className={styles.teamContainer}>
       <h1 className={styles.title}>Meet Our Team</h1>
       <div className={styles.teamGrid}>
+      <Profile
+          name="Jeysson "
+          description="Top 5 people ever."
+          image1="/jason-1.webp"
+          image2="/jason-2.webp"
+        />
         <Profile
           name="Daniel Peterson"
           description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde cumque aliquam, reprehenderit eaque id rem fugiat ea, adipisci libero maiores dolorum sit amet deleniti non doloremque rerum? Officiis, accusamus atque."
